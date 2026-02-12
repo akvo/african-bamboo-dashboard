4a7c188f8925d9cdcacba471be838eb9ae2696c2
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-02-12 16:21:31 +0700

    [#1] feat: Add unit tests for custom serializer fields and health check endpoint

--- backend/utils/custom_serializer_fields.py
+++ backend/utils/custom_serializer_fields.py
@@ -239,93 +239,24 @@ class UnvalidatedField(Field):
         return value
 
 
+def _collect_errors(data, messages, field_name=None):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            _collect_errors(value, messages, field_name=key)
+    elif isinstance(data, list):
+        for item in data:
+            _collect_errors(
+                item, messages, field_name=field_name
+            )
+    elif isinstance(data, str):
+        name = str(field_name) if field_name is not None else ""
+        label = key_map.get(name, name)
+        messages.append(
+            data.replace("field_title", label)
+        )
+
+
 def validate_serializers_message(errors):
-    msg = []
-    if isinstance(errors, dict):
-        for k, v in errors.items():
-            if isinstance(v, list):
-                for val in v:
-                    if isinstance(val, dict):
-                        for ik, iv in val.items():
-                            for viv in iv:
-                                if isinstance(viv, dict):
-                                    for xk, xv in viv.items():
-                                        for xvv in xv:
-                                            msg.append(
-                                                xvv.replace(
-                                                    "field_title",
-                                                    key_map.get(xk, xk),
-                                                )
-                                            )
-                                else:
-                                    msg.append(
-                                        viv.replace(
-                                            "field_title", key_map.get(ik, ik)
-                                        )
-                                    )
-                    else:
-                        msg.append(
-                            val.replace("field_title", key_map.get(k, k))
-                        )
-            else:
-                for k1, v1 in v.items():
-                    if isinstance(v1, list):
-                        for val1 in v1:
-                            msg.append(
-                                val1.replace(
-                                    "field_title",
-                                    key_map.get(str(k1), str(k1)),
-                                )
-                            )
-                    else:
-                        for key2, val2 in v1.items():
-                            for val3 in val2:
-                                msg.append(
-                                    val3.replace(
-                                        "field_title",
-                                        key_map.get(str(k1), str(k1)),
-                                    )
-                                )
-    else:
-        for v in errors:
-            if isinstance(v, list):
-                for val in v:
-                    for ik, iv in val.items():
-                        for viv in iv:
-                            msg.append(
-                                viv.replace("field_title", key_map.get(ik, ik))
-                            )
-
-            else:
-                for k1, v1 in v.items():
-                    if isinstance(v1, dict):
-
-                        for k1k1, v1v1 in v1.items():
-                            for v1v1v in v1v1:
-                                msg.append(
-                                    v1v1v.replace(
-                                        "field_title",
-                                        key_map.get(k1k1, str(k1k1)),
-                                    )
-                                )
-                    else:
-
-                        for val1 in v1:
-                            if isinstance(val1, dict):
-                                for xk1, xv1 in val1.items():
-                                    for xvv1 in xv1:
-                                        msg.append(
-                                            xvv1.replace(
-                                                "field_title",
-                                                key_map.get(xk1, xk1),
-                                            )
-                                        )
-                            else:
-                                msg.append(
-                                    val1.replace(
-                                        "field_title",
-                                        key_map.get(str(k1), str(k1)),
-                                    )
-                                )
-
-    return "|".join(msg)
+    messages = []
+    _collect_errors(errors, messages)
+    return "|".join(messages)

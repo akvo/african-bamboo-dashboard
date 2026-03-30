35d061f61ad972d8e2205d06311ff87e686dcd94
Iwan Firmawan <firmawaneiwan@gmail.com>
2026-03-30 10:56:31 +0700

    [#36] Add allEligible param and available_filters support to useFilterOptions hook

--- frontend/src/hooks/useFilterOptions.js
+++ frontend/src/hooks/useFilterOptions.js
@@ -3,11 +3,12 @@
 import { useState, useEffect } from "react";
 import api from "@/lib/api";
 
-export function useFilterOptions({ formId, region } = {}) {
+export function useFilterOptions({ formId, region, allEligible = false } = {}) {
   const [options, setOptions] = useState({
     regions: [],
     sub_regions: [],
     dynamic_filters: [],
+    available_filters: [],
   });
   const [isLoading, setIsLoading] = useState(false);
 
@@ -17,18 +18,27 @@ export function useFilterOptions({ formId, region } = {}) {
         regions: [],
         sub_regions: [],
         dynamic_filters: [],
+        available_filters: [],
       });
       return;
     }
     setIsLoading(true);
     const params = { form_id: formId };
     if (region) params.region = region;
+    if (allEligible) params.all_eligible = "true";
     api
       .get("/v1/odk/plots/filter_options/", { params })
-      .then((res) => setOptions(res.data))
+      .then((res) =>
+        setOptions({
+          regions: res.data.regions || [],
+          sub_regions: res.data.sub_regions || [],
+          dynamic_filters: res.data.dynamic_filters || [],
+          available_filters: res.data.available_filters || [],
+        }),
+      )
       .catch(() => {})
       .finally(() => setIsLoading(false));
-  }, [formId, region]);
+  }, [formId, region, allEligible]);
 
   return { ...options, isLoading };
 }
